98fce34ba3c5a602102592b3e8725aa777df073c
Rakib Ali Tanzin <tanzin706@gmail.com>
2022-05-12 17:15:57 +0600

    1hour 18 mins

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "welcome to my api!!!"}
+@app.get("/posts")
+def get_post():
+    return{"data": "This is your post"}
+
+@app.post("/createposts")
+def create_post(new_post: Post):
+    print(new_post.published)
+    return{"data": f"new post "}
+
+# title str, content str, category, Bool published
